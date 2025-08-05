415d1ced3c5fdddf0d6d66f5136a59794bc4a666
Sebastian Lütge <sebastian.luetge@tu-dortmund.de>
2025-08-05 16:31:06 +0200

    update python script to conform to object-style

--- plot1.py
+++ plot1.py
@@ -2,7 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(0, 2 * np.pi, 100)
-plt.plot(x, np.sin(x), 'r-')
-plt.xlim(0, 2 * np.pi)
+fig, ax = plt.subplots(1, 1, layout="constrained")
 
-plt.savefig('plot1.pdf')
+ax.plot(x, np.sin(x), "r-", label=r"$\sin(x)$")
+
+ax.set_xlim(0, 2 * np.pi)
+ax.set_ylim(-1.2, 1.2)
+ax.set_xlabel(r"$x$")
+
+ax.set_xticks(
+    np.arange(0, 2.1 * np.pi, np.pi / 2),
+    [
+        r"$0$",
+        r"$\frac{1}{2}\pi$",
+        r"$\pi$",
+        r"$\frac{3}{2}\pi$",
+        r"$2\pi$",
+    ],
+)
+
+ax.legend()
+fig.savefig("plot1.pdf")
--- plot2.py
+++ plot2.py
@@ -2,9 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(0, 2 * np.pi, 100)
-plt.plot(x, np.exp(x), 'r-', label=r'$\exp(x)$')
-plt.xlim(0, 2 * np.pi)
+fig, ax = plt.subplots(1, 1, layout="constrained")
 
-plt.legend()
+ax.plot(x, np.exp(x), "r-", label=r'$\exp(x)$')
 
-plt.savefig('plot2.pdf')
+ax.set_xlim(0, 2 * np.pi)
+ax.set_ylim(-0.2, )
+ax.set_xlabel(r"$x$")
+
+ax.set_xticks(
+    np.arange(0, 2.1 * np.pi, np.pi / 2),
+    [
+        r"$0$",
+        r"$\frac{1}{2}\pi$",
+        r"$\pi$",
+        r"$\frac{3}{2}\pi$",
+        r"$2\pi$",
+    ],
+)
+
+ax.legend()
+fig.savefig("plot2.pdf")
