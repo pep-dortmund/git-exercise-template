41070586ebad3439a5cc8df17aec2a479fb0a6dc
Sebastian Lütge <sebastian.luetge@tu-dortmund.de>
2025-08-06 18:25:50 +0200

    bundle ax settings into a concise block

--- plot1.py
+++ plot1.py
@@ -6,19 +6,20 @@ fig, ax = plt.subplots(1, 1, layout="constrained")
 
 ax.plot(x, np.sin(x), "r-", label=r"$\sin(x)$")
 
-ax.set_xlim(0, 2 * np.pi)
-ax.set_ylim(-1.2, 1.2)
-ax.set_xlabel(r"$x$")
-
-ax.set_xticks(
-    np.arange(0, 2.1 * np.pi, np.pi / 2),
-    [
-        r"$0$",
-        r"$\frac{1}{2}\pi$",
-        r"$\pi$",
-        r"$\frac{3}{2}\pi$",
-        r"$2\pi$",
-    ],
+ax.set(
+    xlim=(0, 2 * np.pi),
+    ylim=(-1.2, 1.2),
+    xlabel=(r"$x$"),
+    xticks=np.arange(0, 2.1 * np.pi, np.pi / 2),
+    xticklabels=(
+        [
+            r"$0$",
+            r"$\frac{1}{2}\pi$",
+            r"$\pi$",
+            r"$\frac{3}{2}\pi$",
+            r"$2\pi$",
+        ]
+    ),
 )
 
 ax.legend()
--- plot2.py
+++ plot2.py
@@ -4,21 +4,22 @@ import matplotlib.pyplot as plt
 x = np.linspace(0, 2 * np.pi, 100)
 fig, ax = plt.subplots(1, 1, layout="constrained")
 
-ax.plot(x, np.exp(x), "r-", label=r'$\exp(x)$')
+ax.plot(x, np.exp(x), "r-", label=r"$\exp(x)$")
 
-ax.set_xlim(0, 2 * np.pi)
-ax.set_ylim(-0.2, )
-ax.set_xlabel(r"$x$")
-
-ax.set_xticks(
-    np.arange(0, 2.1 * np.pi, np.pi / 2),
-    [
-        r"$0$",
-        r"$\frac{1}{2}\pi$",
-        r"$\pi$",
-        r"$\frac{3}{2}\pi$",
-        r"$2\pi$",
-    ],
+ax.set(
+    xlim=(0, 2 * np.pi),
+    ylim=(-0.2, None),
+    xlabel=(r"$x$"),
+    xticks=np.arange(0, 2.1 * np.pi, np.pi / 2),
+    xticklabels=(
+        [
+            r"$0$",
+            r"$\frac{1}{2}\pi$",
+            r"$\pi$",
+            r"$\frac{3}{2}\pi$",
+            r"$2\pi$",
+        ]
+    ),
 )
 
 ax.legend()
